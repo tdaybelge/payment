ef42938fea0ee6bfb3f9b9916ca89c83cf4c40d1
tdaybelge <turhandaybelge@yahoo.com>
2018-08-13 17:11:33 +0300

    money transfer http method switched to PUT

--- money-transfer/routes.js
+++ money-transfer/routes.js
@@ -2,5 +2,5 @@ module.exports = function(app) {
   var transferController = require('./controller');
 
   app.route('/transfers')
-    .post(transferController.transferMoney);
+    .put(transferController.transferMoney);
 };
